f7a46f9f073d674252f2d2931515b1bb6daf1672
ponlponl123 <itsme@ponlponl123.com>
2025-12-15 00:01:14 +0700

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <>
+      <span className="loading loading-spinner loading-xs"></span>
+      <span className="loading loading-spinner loading-sm"></span>
+      <span className="loading loading-spinner loading-md"></span>
+      <span className="loading loading-spinner loading-lg"></span>
+      <span className="loading loading-spinner loading-xl"></span>
+    </>
+  );
+}
